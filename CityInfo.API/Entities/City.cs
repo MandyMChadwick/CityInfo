1b101d58c5cf7f091181f52af6443f3d11de840f
MandyChadwick <cha33dwick@gmail.com>
2024-03-27 11:45:04 -0800

    database seeded

--- CityInfo.API/Entities/City.cs
+++ CityInfo.API/Entities/City.cs
@@ -9,16 +9,21 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+
         [MaxLength(200)]
-        public string Description { get; set; }
-        public ICollection<PointOfInterest> PointOfInterests { get; set; }
-        = new List<PointOfInterest>();
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+               = new List<PointOfInterest>();
+
         public City(string name)
         {
-            Name = name;    
+            Name = name;
         }
     }
 }
+
--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -8,22 +8,21 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
 
         [MaxLength(200)]
-        public string? Description {  get; set; }
-
+        public string? Description { get; set; }
 
         [ForeignKey("CityId")]
         public City? City { get; set; }
         public int CityId { get; set; }
 
-        public PointOfInterest(string Name) {
-
-            Name = Name;
+        public PointOfInterest(string name)
+        {
+            Name = name;
         }
-
     }
 }
